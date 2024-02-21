2059ef384c4d5926232a65160b7aa3b4f14cf8d9
nizar-maatoug <nizar.maatoug@hotmail.com>
2024-02-21 07:24:02 +0100

    shopping-initial-commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let output;
+
+// document.all is deprecated
+output = document.all;
+output = document.all[11];
+output = document.all.length;
+
+// Everything in the html tags
+output = document.documentElement;
+
+// HTMLCollection of everything in head/body
+output = document.head.children;
+output = document.body.children;
+
+// Random properties
+output = document.doctype;
+output = document.domain;
+output = document.URL;
+output = document.characterSet;
+output = document.contentType;
+
+// Get all forms
+output = document.forms;
+output = document.forms[0];
+output = document.forms[0].id;
+output = document.forms[0].method;
+output = document.forms[0].action;
+
+console.log(output);
+
